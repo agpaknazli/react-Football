20056f2bed3d629c219f89df46b9a4c228625772
agpaknazli <agpaknazli19@gmail.com>
2024-09-03 04:28:49 +0200

    header.jsx

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import Container from "react-bootstrap/Container"
-import Header from "./componets/header/Header"
-import LegendContainer from "./components/legends/LegendContainer";
+
 function App() {
   return (
     // <div className="container">
-    //   Hello React
+  <h1>hello</h1>
     // </div>
-    <Container className="text-center my-5">
-      <Header />
-      <LegendContainer />
-    </Container>
-  );
+  )
 }
 export default App;
