80f78491daa05889fd3bd34935ef02153f769ec4
agpaknazli <agpaknazli19@gmail.com>
2024-09-05 21:41:51 +0200

    LegendContainer.jsx

---
+++ src/components/legends/LegendContainer.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+import  Container  from 'react-bootstrap/Container'
+
+const LegendContainer = () => {
+  return (
+    <Container>
+
+      LegendContanier  
+    </Container>
+  )
+}
+
+export default LegendContainer;
