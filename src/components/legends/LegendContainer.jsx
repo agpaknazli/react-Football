43cb2475d21389d16f49bdbe12116333c1670f78
agpaknazli <agpaknazli19@gmail.com>
2024-09-09 21:47:48 +0200

    input

--- src/components/legends/LegendContainer.jsx
+++ src/components/legends/LegendContainer.jsx
@@ -8,20 +8,31 @@ import {data} from "../../helpers/data";
 import {CardFooter} from "react-bootstrap";
 import { useState } from "react";
 import LegendCard from "./LegendCard";
+import Form from 'react-bootstrap/Form';
+
 
 const LegendContainer = () => {
 
     const [show,setShow]=useState(true);
 
   return (
+
+<>
+<Form.Control type="email" placeholder="name@example.com" />
+
     <Container>
       <Row>
         {data.map((legend)=> (
         <LegendCard key={legend.id} legend={legend}/>
         ))}
+
+{/* {data.map((legend)=>(<LegendCard key={legend.id}{...legend}/>
+))} */}
+
       </Row>
      
     </Container>
+    </>
   );
 };
 
